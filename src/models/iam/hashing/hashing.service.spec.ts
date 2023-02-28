8996e18f15555dde0f8cd91752899fa31dd49464
beyerenrico <enrico.beyer@synaigy.com>
2023-02-28 11:33:20 +0100

    :heavy_check_mark: Change providers array

--- src/models/iam/hashing/hashing.service.spec.ts
+++ src/models/iam/hashing/hashing.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { BcryptService } from './bcrypt.service';
 import { HashingService } from './hashing.service';
 
 describe('HashingService', () => {
@@ -7,7 +8,12 @@ describe('HashingService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HashingService],
+      providers: [
+        {
+          provide: HashingService,
+          useClass: BcryptService,
+        },
+      ],
     }).compile();
 
     service = module.get<HashingService>(HashingService);
