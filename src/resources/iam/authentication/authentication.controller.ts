e3f775cb6ffa1d39be298682b1c89259791bf2aa
beyerenrico <enrico.beyer@synaigy.com>
2023-02-28 15:45:20 +0100

    :sparkles: Add method to refresh tokens

--- src/resources/iam/authentication/authentication.controller.ts
+++ src/resources/iam/authentication/authentication.controller.ts
@@ -4,6 +4,7 @@ import { Auth } from '../decorators/auth.decorator';
 
 import { AuthenticationService } from './authentication.service';
 
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthType } from './enums/auth-type.enum';
@@ -22,5 +23,11 @@ export class AuthenticationController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('refresh-tokens')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
